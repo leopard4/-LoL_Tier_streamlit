c63f616ebe5a9b5b90f79ada732fb31214ca4515
leopard4 <95200973+leopard4@users.noreply.github.com>
2022-12-22 22:12:17 +0900

    이미지 사이즈조절 등

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import plotly.express as px
-import altair as alt  
 from sklearn.preprocessing import LabelEncoder
+from PIL import Image
+import io
+import requests
 
 from pyparsing import empty
 from tkinter.tix import COLUMN
@@ -22,6 +23,8 @@ plt.rcParams['axes.unicode_minus'] = False
 if platform.system() == 'Linux':
     rc('font', family='NanumGothic')
 
+
+
 # 레이아웃 설정
 st.set_page_config(layout="wide")
 empty1,con1,empty2 = st.columns([0.2,0.5,0.2])
@@ -31,12 +34,22 @@ empyt1,con4_1,empty2 = st.columns([0.3,0.7,0.3])
 empyt1,con5,con6,empty2 = st.columns([0.3,0.35,0.35,0.3])
 empyt1,con6_1,empty2 = st.columns([0.3,0.7,0.3])
 empyt1,con7,con8,empty2 = st.columns([0.3,0.35,0.35,0.3])
+empyt1,con8_1,empty2 = st.columns([0.3,0.7,0.3])
 
+# 이미지 크기조정을 위한 함수
 
 def main() :
     
     df = pd.read_csv('data/League of Legends Champion Stats 12.1.csv', sep =';')
     df.dropna(inplace=True)
+    # 이름 가공
+    df['Name'] = df['Name'].str.replace(' ', '')
+    df['Name'] = df['Name'].str.replace('\'', '')
+    df['Name'] = df['Name'].str.replace('.', '')
+    for i in df['Name'].unique():
+        st.sidebar.image(f'https://opgg-static.akamaized.net/meta/images/lol/champion/{i}.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto,f_webp,w_160&v=1671624147144')
+        st.sidebar.text(i)
+    
 
     # 차트를 위한 레이블링
     df1= df.copy()
@@ -49,15 +62,20 @@ def main() :
         empty()# 좌 여백부분1
         
     with con1 :
-        st.image('https://cloudfront-us-east-1.images.arcpublishing.com/infobae/YABJ7CAXOZDVHAXSDRSQQ7NJR4.jpg')
+        url = 'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/YABJ7CAXOZDVHAXSDRSQQ7NJR4.jpg'
+        response = requests.get(url)
+        image_data = response.content
+        image = Image.open(io.BytesIO(image_data))
+        image = image.resize((1200, 300),resample=Image.BICUBIC)
+        st.image(image)
         st.header('League of Legends Champion Stats 12.1')
         # st.area_chart(df['Win %'].head(3))
-        
-
+        # 'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/YABJ7CAXOZDVHAXSDRSQQ7NJR4.jpg'
     with con2 :
         
         choice = st.radio('해당티어만 보기', ['전체', 'God','S', 'A', 'B', 'C', 'D'])
         character = st.text_input('캐릭터 풀네임을 검색해보세요', 'Search')
+        st.write("이름이 기억이 나지않는다면 좌측 슬라이드바를 확인하세요")
         # 해당 영웅의 이미지 예정
     
     with con3 :
@@ -82,11 +100,23 @@ def main() :
         
     with con4:
         if character != 'Search' and character != '':
-            hero_selceted = st.selectbox('다른 영웅을 선택하고 싶다면 검색을 비우세요.', df[df['Name'] == character.capitalize()])
-            st.image(f'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/{hero_selceted}_0.jpg')
+            hero_selceted = st.selectbox('다른 영웅을 선택하고 싶다면, 최근 검색을 비워야만 합니다.', df[df['Name'] == character.capitalize()])
+            url = f'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/{hero_selceted}_0.jpg'
+            response = requests.get(url)
+            image_data = response.content
+            image = Image.open(io.BytesIO(image_data))
+            image = image.resize((1200, 300),resample=Image.BICUBIC)
+            st.image(image)
+            
         else:
             hero_selceted = st.selectbox('영웅을 골라보세요', df['Name'].unique())
-            st.image(f'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/{hero_selceted}_0.jpg')
+            url = f'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/{hero_selceted}_0.jpg'
+            response = requests.get(url)
+            image_data = response.content
+            image = Image.open(io.BytesIO(image_data))
+            image = image.resize((1200, 300),resample=Image.BICUBIC)
+            st.image(image)
+           
         
        
     with con4_1:
@@ -102,13 +132,11 @@ def main() :
 
     with con5 :
     
-        # 선택한 영웅의 데이터를 pie chart로 표현
-        pass
-       
+        # 선택한 영웅의 데이터를 pie chart로 표
 
-        
-        # fig = px.line(df, x='Name', y='Win %', title='Win %')
-        # fig.add_scatter(x=df['Name'], y=df[df['Name'] == hero_selceted]['Win %'], mode='lines', name=hero_selceted)
+        pass
+        # fig = plt.figure()
+        # plt.scatter(x=df['Name'], y=df[df['Name'] == hero_selceted]['Win %'], mode='lines', name=hero_selceted)
         # st.plotly_chart(fig)
 
 
@@ -136,7 +164,9 @@ def main() :
         ''')
         st.text('클래스별 트렌드')
         st.bar_chart(df1.groupby('Class')['Trend'].mean())
-		
+        st.dataframe(df1.groupby('Class')['Trend'].mean().sort_values(ascending=False))  
+
+
         st.markdown('''
         -----------
         ''')
@@ -149,10 +179,23 @@ def main() :
         ''')
         st.text('클래스별 스코어')
         st.bar_chart(df1.groupby('Class')['Score'].mean())
+        st.dataframe(df1.groupby('Class')['Score'].mean().sort_values(ascending=False))  
 
         st.markdown('''
         -----------
         ''')
+    with con8_1 :
+        if st.button('<<<<<<<상관관계 분석 버튼>>>>>>>>>>') :                
+            fig = plt.figure()
+            sns.heatmap(df1.corr(), annot=True, cmap='coolwarm', center=0, fmt='.1f', linewidths=1, linecolor='white')
+            plt.title('상관관계')
+            st.pyplot(fig)
+            st.write('분석결과 Score와 pick % 은 아주 강한 상관관계에 있음을 알 수 있습니다.')
+        
+
+    
+
+
     with empty2 :
        pass
 
