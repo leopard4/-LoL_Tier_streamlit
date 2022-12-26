64359b642e4cc0a2fe3b43dcb03e698398c67ad7
leopard4 <95200973+leopard4@users.noreply.github.com>
2022-12-26 13:16:41 +0900

    기능을 설명하는 주석을 추가함

--- app.py
+++ app.py
@@ -3,20 +3,18 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
 from PIL import Image
 import io
 import requests
 from pyparsing import empty
-import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-# from tkinter.tix import COLUMN
 
 # 이 코드는 ec2에 한글 폰트가 설치되어 있어야 하고,
 # 파이썬에서 한글 사용가능토록 먼저 셋팅해야 한다.
 # https://luvris2.tistory.com/119#1.4.%20matplotlib%EC%97%90%20%ED%95%9C%EA%B8%80%20%ED%8F%B0%ED%8A%B8%20%ED%99%95%EC%9D%B8
 
+# 리눅스에서 한글처리를 위한 코드
 import platform
 from matplotlib import font_manager, rc
 plt.rcParams['axes.unicode_minus'] = False
@@ -36,13 +34,14 @@ empyt1,con6_1,empty2 = st.columns([0.3,0.7,0.3])
 empyt1,con7,con8,empty2 = st.columns([0.3,0.35,0.35,0.3])
 empyt1,con8_1,empty2 = st.columns([0.3,0.7,0.3])
 
-# 이미지 크기조정을 위한 함수
+
 
 def main() :
     
+    # 데이터를 가져와서 null 처리
     df = pd.read_csv('data/League of Legends Champion Stats 12.1.csv', sep =';')
     df.dropna(inplace=True)
-    # 이름 가공
+    # 사이드 이미지를 가져오기 위한 코드
     df['Name'] = df['Name'].str.replace(' ', '')
     df['Name'] = df['Name'].str.replace('\'', '')
     df['Name'] = df['Name'].str.replace('.', '')
@@ -51,17 +50,20 @@ def main() :
         st.sidebar.text(i)
     
 
-    # 차트를 위한 레이블링
+    # 차트를 그리기위한 숫자로 만드는 코드
     df1= df.copy()
     df1['Win %']=df1['Win %'].str.replace('%', '').astype(float) 
     df1['Role %']=df1['Role %'].str.replace('%', '').astype(float) 
     df1['Pick %']=df1['Pick %'].str.replace('%', '').astype(float) 
     df1['Ban %']=df1['Ban %'].str.replace('%', '').astype(float) 
+    
     with empty1 :
         
         empty()# 좌 여백부분1
         
     with con1 :
+        
+        # 타이틀 이미지와 제목
         url = 'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/YABJ7CAXOZDVHAXSDRSQQ7NJR4.jpg'
         response = requests.get(url)
         image_data = response.content
@@ -69,17 +71,17 @@ def main() :
         image = image.resize((1200, 300),resample=Image.BICUBIC)
         st.image(image)
         st.header('League of Legends Champion Stats 12.1')
-        # st.area_chart(df['Win %'].head(3))
-        # 'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/YABJ7CAXOZDVHAXSDRSQQ7NJR4.jpg'
+       
     with con2 :
         
+        # 티어 고르는 라디오 버튼 / 검색창
         choice = st.radio('해당티어만 보기', ['전체', 'God','S', 'A', 'B', 'C', 'D'])
         character = st.text_input('캐릭터 풀네임을 검색해보세요', 'Search')
         st.write("이름이 기억이 나지않는다면 좌측 슬라이드바를 확인하세요")
-        # 해당 영웅의 이미지 예정
-    
+       
     with con3 :
-
+        
+        # 고른 티어를 데이터 프레임으로 보여줌
         if choice == '전체' :
             st.dataframe(df)
         elif choice == 'God' :
@@ -99,6 +101,8 @@ def main() :
             st.dataframe(df[df['Name'] == character.capitalize()])
         
     with con4:
+
+        # 검색한 캐릭터의 이미지를 가져와서 보여줌
         if character != 'Search' and character != '':
             hero_selceted = st.selectbox('다른 영웅을 선택하고 싶다면, 최근 검색을 비워야만 합니다.', df[df['Name'] == character.capitalize()])
             url = f'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/{hero_selceted}_0.jpg'
@@ -112,6 +116,7 @@ def main() :
                 st.image(image)
             
         else:
+            
             hero_selceted = st.selectbox('영웅을 골라보세요', df['Name'].unique())
             url = f'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/{hero_selceted}_0.jpg'
             response = requests.get(url)
@@ -127,6 +132,8 @@ def main() :
        
     with con4_1:
         
+        # 선택한 데이터에 동일한 영웅이 존재할수있으므로
+        # 그중 첫번째 행만 가져옴
         hero = df1.loc[ df1['Name'] == hero_selceted]
         Role_group = df1.groupby('Role')[['Win %','Score','Trend','KDA']].agg(np.mean).reset_index()
         hero_val = hero['Role'].values[0]
@@ -139,10 +146,10 @@ def main() :
         choose_role.drop('Role',axis=1,inplace=True)
         last_df = pd.concat([hero, choose_role], axis=0)
         last_df.reset_index(inplace=True)
-        #st.dataframe(last_df)
 
     with con5 :
         
+        # 선택한 영웅의 데이터를 서브플롯으로 보여줌
         fig = make_subplots(rows=2, cols=2,subplot_titles=last_df.columns[1:])
         fig.add_trace(
             go.Scatter(x=last_df['index'], y=last_df['Score'],name="Score",
@@ -181,7 +188,6 @@ def main() :
 
         fig.update_layout(height=600, width=800,legend_title="Legend", title_text="선택한 영웅과 같은 포지션의 평균 스탯을 비교합니다")
 
-
         fig.update_xaxes(title_text="영웅점수", range=[-1,2],row=1, col=1)
         fig.update_xaxes(title_text="트렌드점수", range=[-1,2], row=1, col=2)
         fig.update_xaxes(title_text="승률", range=[-1,2], showgrid=False, row=2, col=1)
@@ -200,6 +206,7 @@ def main() :
 
     with con7 :
         
+        # 클래스 카테고리 평균값 차트 
         st.markdown('''
         -----------
         ''')
@@ -207,9 +214,6 @@ def main() :
         st.bar_chart(df1.groupby('Class')['Trend'].mean())
         st.dataframe(df1.groupby('Class')['Trend'].mean().sort_values(ascending=False)) 
         
-         
-
-
         st.markdown('''
         -----------
         ''')
@@ -217,14 +221,13 @@ def main() :
 
        
     with con8 :
+
         st.markdown('''
         -----------
         ''')
         st.text('클래스별 스코어')
         st.bar_chart(df1.groupby('Class')['Score'].mean())
         st.dataframe(df1.groupby('Class')['Score'].mean().sort_values(ascending=False))  
-        # fig6 = px.pie(df1, 'Class','Role',title= '클래스별 스코어')
-        # st.plotly_chart(fig6)
 
         st.markdown('''
         -----------
@@ -232,10 +235,13 @@ def main() :
 
         
     with con8_1 :
+
+        # 선택한 영웅과 비슷한 추천영웅 데이터 프레임으로 표시
         st.subheader('선택한 영웅과 같은 포지션의 좋은 영웅을 추천합니다.')
         suggestion = df1.loc[ df1['Role'] == hero_val, ]
         st.dataframe(suggestion.sort_values('Score',ascending=False).head(5))
 
+        # 버튼을 누르면 상관관계를 표시함
         if st.button('더 알고싶다면 누르세요') :     
             st.write('1에 가까울수록 비례관계 -1에 가까울수록 반비례 관계를 하나의 표로 표현한 것입니다.')           
             fig = plt.figure()
@@ -246,15 +252,8 @@ def main() :
             st.write('Score와 ban % 은 비례관계임을 알수있습니다.')
             st.write('결론적으로 Score가 높을수록 게임에 영향이 큰 캐릭터라고 유추할수있습니다.')
 
-       
-    
-
-
     with empty2 :
        pass
-
-
-
 
 
 if __name__ == '__main__' :
